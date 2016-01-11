1d981569cdf7bb298faea7f7ff2034874ebbede8
Eitan Sternlicht <eitanoffun@gmail.com>
2016-01-11 17:19:25 +0200

    add ILS add buying/selling

--- Exchange.elm
+++ Exchange.elm
@@ -3,7 +3,112 @@ module Exchange where
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Effects
+import StartApp.Simple as StartApp
+import Json.Decode exposing (..)
+-- MODEL
 
+type alias Model =
+  { leftInput : String
+  , rightInput : String
+  }
+
+
+initialModel : Model
+initialModel =
+  { leftInput = ""
+  , rightInput = ""
+  }
+
+
+-- UPDATE
+
+type Action
+  = NoOp
+  | UpdateLeftInput
+  | UpdateRightInput
+
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    NoOp ->
+      model
+
+    UpdateRightInput ->
+      model
+
+    UpdateLeftInput ->
+      model
+
+
+
+
+-- PORTS
+
+port lastPrice : Signal String
+
+-- VIEW
+
+
+-- apiResponse =
+
+
+
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div
+    [ id "container" ]
+    [ pageHeader
+    , pageFooter
+    ]
+
+
+pageHeader : Html
+pageHeader =
+  h1 [ ] [ text "Convert BTC" ]
+
+
+pageFooter : Html
+pageFooter =
+  footer [ ]
+    [ a
+      [ href "https://www.eitansternlicht.com" ]
+      [ text "Created By Eitan Sternlicht" ] ]
+
+val : Decoder String
+val =
+    at ["ticker", "last"] string
+
+parseLastPrice json =
+  case decodeString val json of
+    Ok value ->
+      value
+    Err error ->
+      ""
+
+toStringText val =
+  text (toString val)
+
+-- port refreshLastPrice : Signal Bool
+-- port refreshLastPrice =
+-- value : Decoder String
+-- value =
+--     at List String Json.Decode.Decoder a  string
+
+-- refreshButton =
+--   button
+--     [  ]
+--     [ text "Refresh Last Price"]
 
 main =
-  text "Elm is here! yay!!"
+  Signal.map text (Signal.map parseLastPrice lastPrice)
+
+-- main: Signal Html
+-- main =
+--   StartApp.start
+--     { model = initialModel,
+--       view = view,
+--       update = update
+--     }
--- main.js
+++ main.js
@@ -1,35 +1,85 @@
-var lastPrice;
+var idrPrice;
+var ilsPrice;
 
-function refreshLastPrice() {
+
+function getIlsRate() {
+  if (isBuying())
+    return ilsPrice.buy;
+  return ilsPrice.sell;
+}
+function isBuying() {
+  return document.getElementById("buying").checked;
+}
+
+
+function refreshIDR() {
   $lastPrice = $('#lastPrice');
   $lastPrice.html("");
   $.get("https://vip.bitcoin.co.id/api/btc_idr/ticker")
     .then(function (data) {
-      lastPrice = JSON.parse(data).ticker.last - 100000
-      $lastPrice.html(lastPrice);
+      idrPrice = JSON.parse(data).ticker.last - 100000
+      // $lastPrice.html(lastPrice);
+      // app.ports.lastPrice.send("" + lastPrice);
+      // app.ports.lastPrice.send("" + data);
   });
 }
 
-refreshLastPrice();
+function refreshILS() {
+  // $.get("https://www.bitsofgold.co.il/api/btc")
+  //   .then(function (data) {
+  //       ilsPrice = JSON.parse(data);
+  //   });
+  ilsPrice = {"buy": 1833.7596014713597, "sell": 1664.5540186445937 }
+}
+
+function refreshRates() {
+  refreshIDR();
+  refreshILS();
+}
+
+refreshRates();
+setInterval(refreshRates, 60 * 1000);
+
 
 $('#btc').keyup(function() {
     btcVal = parseFloat($(this).val());
     if (btcVal) {
-      idrVal = btcVal * lastPrice
+      idrVal = btcVal * idrPrice;
       $('#idr').val(parseInt(idrVal));
+      ilsVal = btcVal * ((ilsPrice.buy + ilsPrice.sell) / 2);
+      $('#ils').val(parseInt(ilsVal));
     }
     else {
-      $('#idr').val("")
+      $('#idr').val("");
+      $('#ils').val("");
+
     }
 });
 
 $('#idr').keyup(function() {
     idrVal = parseInt($(this).val());
     if (idrVal) {
-      btcVal = idrVal / lastPrice;
+      btcVal = idrVal / idrPrice;
       $('#btc').val(btcVal.toFixed(8));
+      ilsVal = btcVal * getIlsRate();
+      $('#ils').val(parseInt(ilsVal));
     }
     else {
       $('#btc').val("")
+      $('#ils').val("")
+    }
+});
+
+$('#ils').keyup(function() {
+    ilsVal = parseInt($(this).val());
+    if (ilsVal) {
+      btcVal = ilsVal / getIlsRate();
+      $('#btc').val(btcVal.toFixed(8));
+      idrVal = btcVal * idrPrice;
+      $('#idr').val(parseInt(idrVal));
+    }
+    else {
+      $('#btc').val("");
+      $('#idr').val("");
     }
 });
